b92e537b6017827999045798ced1f96055e225c2
Akinsete Akinwale <akfreshman3@icloud.com>
2022-03-31 22:11:48 -0600

    second commit all added

--- main.tf
+++ main.tf
@@ -13,5 +13,145 @@ provider "aws" {
 }
 
 variable "region" {}
+variable "vpc_cidr" {}
+variable "env" {}
+variable "private_subnet_cidr" {}
+variable "public_subnet_cidr" {}
+variable "instance_type" {}
 
+variable "avail_zone" {}
+
+//vpc
+resource "aws_vpc" "myapp-vpc" {
+  cidr_block = var.vpc_cidr
+  tags = {
+    Name = "${var.env}-vpc"
+  }
+}
+
+resource "aws_subnet" "myapp_public_subnet" {
+  vpc_id = aws_vpc.myapp-vpc.id
+  cidr_block = var.public_subnet_cidr
+  availability_zone = var.avail_zone
+  tags = {
+    Name = "${var.env}-public_subnet"
+  }
+}
+
+resource "aws_subnet" "myapp_private_subnet" {
+  vpc_id = aws_vpc.myapp-vpc.id
+  cidr_block = var.private_subnet_cidr
+  availability_zone = var.avail_zone
+  tags = {
+    Name = "${var.env}-private_subnet"
+  }
+}
+
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.myapp-vpc.id
+
+  tags = {
+    Name = "${var.env}-igw"
+  }
+}
+
+resource "aws_route_table" "public_myapp-route-table" {
+  vpc_id = aws_vpc.myapp-vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+
+  tags = {
+    Name = "${var.env}-route-table"
+  }
+}
+
+resource "aws_route_table_association" "public_subnet_association" {
+  route_table_id = aws_route_table.public_myapp-route-table.id
+  subnet_id = aws_subnet.myapp_public_subnet.id
+}
+
+resource "aws_route_table" "private_myapp-route-table" {
+  vpc_id = aws_vpc.myapp-vpc.id
+
+  tags = {
+    Name = "${var.env}-route-table"
+  }
+}
+
+resource "aws_route_table_association" "private_subnet_association" {
+  route_table_id = aws_route_table.private_myapp-route-table.id
+  subnet_id = aws_subnet.myapp_private_subnet.id
+}
+
+resource "aws_security_group" "public_ec2_sg" {
+  name        = "${var.env}-public_ec2-sg"
+  description = "Allow SSH and HTTP"
+  vpc_id      = aws_vpc.myapp-vpc.id
+
+  ingress {
+    description      = "SSH to VPC"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description      = "HTTP to VPC"
+    from_port        = 8080
+    to_port          = 8080
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "${var.env}-public_ec2_sg"
+  }
+}
+
+data "aws_ami" "latest_ami_image" {
+  most_recent      = true
+#  name_regex       = "^amzn2-ami-hvm-\\d{3}"
+  owners           = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+  }
+
+  filter {
+    name   = "root-device-type"
+    values = ["ebs"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+
+resource "aws_instance" "public_ec2" {
+  ami = data.aws_ami.latest_ami_image.id
+  instance_type = var.instance_type
+
+  subnet_id = aws_subnet.myapp_public_subnet.id
+  vpc_security_group_ids = [aws_security_group.public_ec2_sg.id]
+  availability_zone = var.avail_zone
+  associate_public_ip_address = true
+  key_name = "dev-key"
+
+  tags = {
+    Name = "${var.env}-public_ec2"
+  }
+}
 
--- terraform.tfvars
+++ terraform.tfvars
@@ -1,1 +1,7 @@
 region="us-west-2"
+vpc_cidr = "10.0.0.0/16"
+env="dev"
+private_subnet_cidr = "10.0.2.0/24"
+public_subnet_cidr = "10.0.1.0/24"
+instance_type="t2.micro"
+avail_zone="us-west-2a"
