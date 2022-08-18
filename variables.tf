1fc1dcd2656520aaa1f5ab3642416f9abbf52f1a
ayoshittu <55026063+shitboi@users.noreply.github.com>
2022-08-18 00:07:06 -0400

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -9,8 +9,8 @@ variable "public_subnet_cidr" {}
 variable "instance_type" {}
 variable "avail_zone" {}
 variable "aws_access_key" {
-  default = "AKIASGS7EDL6OSGVHAGE"
+  default = ""
 }
 variable "aws_secret_key" {
-  default = "dB6bQkLrfS5uT/Nlh46zvEOhQoI0jGgyQSUTw531"
+  default = ""
 }
